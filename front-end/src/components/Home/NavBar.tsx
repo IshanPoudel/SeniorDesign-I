47335339a6a644c10f88122edd87e9a7440d0737
HuyNguyenUTA <huy.nguyen.uta@gmail.com>
2024-01-30 11:40:33 -0600

    Func: Reload page when logo is clicked

--- front-end/src/components/Home/NavBar.tsx
+++ front-end/src/components/Home/NavBar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from 'next/image'
 import Logo from "../../images/trademinds_transparent.png";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
     return (
         <div className="h-20 bg-zinc-50 dark:bg-black">
             <div className="h-full flex flex-1 justify-end md:justify-center items-center">
-                <div className="ml-5">
-                    <Image src={Logo} alt="Logo" width={60} height={60}></Image>
+                <div className="ml-5 w-fit pointer-events-auto cursor-pointer">
+                    <a href="/">
+                        <img src={Logo.src} alt="Logo" width={60} height={60} ></img>
+                    </a>
                 </div>
                 <div className="flex flex-1 justify-center items-center">
                     <nav className="pointer-events-auto hidden md:block">
